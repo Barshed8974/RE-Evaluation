a3c962913982eee27b67023c38f690f30a70c10f
Ali <Barshed8974>
2021-09-24 16:49:20 +0530

    Commit3

--- app/src/main/java/com/example/myapplication/SneakerFragment.kt
+++ app/src/main/java/com/example/myapplication/SneakerFragment.kt
@@ -5,28 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [SneakerFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SneakerFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private  var reponseList: List<ResponseDTO!>! = listOf<ResponseDTO>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -34,26 +23,28 @@ class SneakerFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sneaker, container, false)
+        var  view= inflater.inflate(R.layout.fragment_sneaker, container, false)
+        callApi()
+        return view
+
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SneakerFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SneakerFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    private fun callApi() {
+        val apiServices=Network.getRetrofit().create(ApiServices::class.java)
+        apiServices.getResponse().enqueue(object : Callback<List<ResponseDTO>>
+        {
+            override fun onResponse(
+                call: Call<List<ResponseDTO>>,
+                response: Response<List<ResponseDTO>>
+            ) {
+                TODO("Not yet implemented")
             }
+
+            override fun onFailure(call: Call<List<ResponseDTO>>, t: Throwable) {
+                TODO("Not yet implemented")
+            }
+
+        })
     }
+
 }
